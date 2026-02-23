76cb24b08cd11d920dc5d0294c7744f9723d8012
Tithi Shah <tithishah26@gmail.com>
2026-02-23 13:18:46 +0530

    fix(frontend): update express routing and remove regex error

--- frontend/server.js
+++ frontend/server.js
@@ -8,10 +8,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 5006;
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
